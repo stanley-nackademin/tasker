11cef328a32909eb1cc5abfdb56574574cc4e400
stanley <stanley.ho@yh.nackademin.se>
2018-05-23 10:00:29 +0200

    Very small formatting fix

--- src/main/java/se/group/backendgruppuppgift/tasker/resource/TeamResource.java
+++ src/main/java/se/group/backendgruppuppgift/tasker/resource/TeamResource.java
@@ -1,6 +1,5 @@
 package se.group.backendgruppuppgift.tasker.resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import se.group.backendgruppuppgift.tasker.model.Team;
 import se.group.backendgruppuppgift.tasker.model.User;
